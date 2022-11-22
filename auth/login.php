2d926086c76c1d36b1dd72993643d01c691b813d
Avin Divakara <divakaraavin@gmail.com>
2022-11-22 17:53:26 +0530

    Login page initialization

---
+++ auth/login.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <script type="text/javascript" src="../vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+  <link href="../vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
+  <link href="../assets/css/master.css" rel="stylesheet"/>
+  <link href="../assets/css/login.css" rel="stylesheet"/>
+  <title>Login | Pixihire</title>
+</head>
+</html>
