c0969471a5510bc3537dbc69c8af72c3b815d9e7
Avin Divakara <divakaraavin@gmail.com>
2022-12-26 17:47:04 +0530

    Signup page edits, with create account option

--- auth/signup.php
+++ auth/signup.php
@@ -1,61 +1,104 @@
 <?php
-include '../components/links.php';
-$link = new Links();
+    session_start();
+    include '../components/links.php';
+    include_once '../components/page_processing.php';
+    include_once '../backend/account.php';
+    // Initializations
+    $pp = new page_processor();
+    $link = new Links();
+    $controller = new Account();
+    $error_code = 0;
+    // Program
+    $pp->is_logged_in($_COOKIE); // Check if user is logged in
+    // If logged in, redirect to dashboard page
+    if($pp->logged_in){
+        header("Location: ".$link->path('dashboard_page')); // Redirect to dashboard page
+        die();
+    }
+    // If not logged in, Can create a new account
+    // Check if form is submitted
+    if(!empty($_POST)){
+        // Form is submitted
+        // Get all the data
+        $firstname = $_POST['fname'];
+        $lastname = $_POST['lname'];
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $phone_no = $_POST['phone'];
+        $dob = $_POST['dob'];
+        $password = $_POST['pass'];
+        $confirm_password = $_POST['cpass'];
+        // Create account
+        $result = $controller->create_account($firstname, $lastname, $username, $email, $phone_no, $dob, $password, $confirm_password);
+        if ($result===0){
+            // Account created successfully
+            // Redirect to login page
+            header("Location: ".$link->path('email_verification_page')); // Redirect to verification page
+            die();
+        } elseif($result>0) $error_code = $result;
+    }
+
 ?>
 <html lang="en">
 <head>
     <?php
     include '../components/essentials.php'
     ?>
-    <link href="../assets/css/signup.css" rel="stylesheet"/>
+    <link href="<?php echo $link->path('signup_css'); ?>" rel="stylesheet"/>
     <title>Signup | Pixihire</title>
 </head>
 <body>
-<div class="d-xxl-flex">
-    <div class="col-xxl-5 d-flex justify-content-md-center justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center align-items-xxl-center" id="imageCol" style="background: var(--bs-white);"><img class="d-xxl-flex justify-content-xxl-center align-items-xxl-center bgArt" src="<?php echo $link->path('signup_img'); ?>" width="605" height="437" style="margin-left: 89px;"></div>
+<div id="signup" class="d-xxl-flex">
+    <div id="imageCol" style="background-image: url(<?php echo $link->path('signup_img'); ?>);">
+    </div>
     <div class="col d-xl-flex d-xxl-flex align-items-xxl-center">
         <div class="container">
-            <h1 class="d-flex justify-content-center mainHead" style="text-align: center;color: #051B3B;">Join Pixihire.</h1>
+            <h1 class="d-flex justify-content-center mainHead" style="text-align: center;color: #051B3B;margin-top: 30px;">Join Pixihire.</h1>
             <h1 class="secondHead" style="margin-top: 40px;">Sign up with</h1>
-            <div class="d-xxl-flex justify-content-xxl-center align-items-xxl-center buttonContainer"><a class="sm" href="#google" style="padding-bottom: 0px;"><button class="btn btn-primary d-xxl-flex justify-content-xxl-center align-items-xxl-center smBtn" type="button"><img class="d-xxl-flex align-items-xxl-center icon" src="assets/img/google.png" width="22" height="22">Google</button></a><a class="sm" href="#facebook" style="padding-bottom: 0px;"><button class="btn btn-primary d-xxl-flex justify-content-xxl-center align-items-xxl-center smBtn" type="button" style="width: 160px;"><img class="d-xxl-flex align-items-xxl-center icon" src="assets/img/facebook.png" width="22" height="22">Facebook</button></a><a class="sm" href="#apple" style="padding-bottom: 0px;"><button class="btn btn-primary d-xxl-flex justify-content-xxl-center align-items-xxl-center smBtn" type="button"><img class="d-xxl-flex align-items-xxl-center icon" src="assets/img/apple.png" width="22" height="22">Apple</button></a></div>
+            <div class="d-xxl-flex justify-content-xxl-center align-items-xxl-center buttonContainer">
+                <a class="sm" href="#" style="padding-bottom: 0px;"><button class="btn btn-primary d-xxl-flex justify-content-xxl-center align-items-xxl-center smBtn" type="button"><i class="fa-brands fa-google" style="font-size: 20px;"></i>Google</button></a>
+                <a class="sm" href="#" style="padding-bottom: 0px;"><button class="btn btn-primary d-xxl-flex justify-content-xxl-center align-items-xxl-center smBtn" type="button" style="width: 160px;"><i class="fa-brands fa-facebook" style="font-size: 22px;"></i>Facebook</button></a>
+                <a class="sm" href="#" style="padding-bottom: 0px;"><button class="btn btn-primary d-xxl-flex justify-content-xxl-center align-items-xxl-center smBtn" type="button"><i class="fa-brands fa-apple" style="font-size: 22px;"></i>Apple</button></a></div>
             <h1 class="d-flex d-xxl-flex justify-content-center justify-content-xxl-center secondHead" style="margin-top: 30px;">Or</h1>
             <h1 class="secondHead" style="margin-top: 40px;margin-bottom: 20px;">Create account by giving these details</h1>
             <p class="about">Give accurate information as it will be taken for futher verifications of your profile.<br></p>
             <h1 class="secondHead" style="margin-top: 40px;margin-bottom: 4px;font-size: 14px;"><span style="color: rgb(255, 0, 0);">*</span> for required fields</h1>
-            <form class="signupForm" method="GET">
-                <div class="table-responsive" style="width: 550px;overflow: hidden;">
-                    <table class="table">
-                        <thead>
-                        <tr></tr>
-                        </thead>
-                        <tbody style="width: 374px;">
-                        <tr class="d-xxl-flex justify-content-xxl-start" style="width: 460px;">
-                            <td style="width: 230px;"><label class="form-label inputHead">First Name *<input class="form-control inputf" type="text" placeholder="First Name *" name="fname" required="" style="width: 185px;"></label></td>
-                            <td><label class="form-label inputHead">Last Name *<input class="form-control inputf" type="text" placeholder="Last Name *" name="lname" required="" style="width: 185px;"></label></td>
-                        </tr>
-                        <tr class="d-xxl-flex justify-content-xxl-start" style="width: 460px;">
-                            <td colspan="2" style="width: 230px;"><label class="form-label inputHead">Email *<input class="form-control inputf" type="email" placeholder="Email *" name="email" required="" style="width: 415px;"></label></td>
-                        </tr>
-                        <tr class="d-xxl-flex justify-content-xxl-start" style="width: 460px;">
-                            <td style="width: 230px;"><label class="form-label inputHead">Password *<input class="form-control inputf" type="password" placeholder="Password *" name="pass" required="" style="width: 185px;"></label></td>
-                            <td><label class="form-label inputHead">Confirm Password *<input class="form-control inputf" type="password" placeholder="Confirm Password *" name="cpass" required="" style="width: 185px;"></label></td>
-                        </tr>
-                        <tr class="d-xxl-flex justify-content-xxl-start" style="width: 460px;">
-                            <td colspan="2" style="width: 230px;"><label class="form-label inputHead">Email *<input class="form-control inputf" type="email" placeholder="Email *" name="email" required="" style="width: 415px;"></label></td>
-                        </tr>
-                        <tr class="d-xxl-flex justify-content-xxl-start" style="width: 460px;">
-                            <td style="width: 230px;"><label class="form-label inputHead">Password *<input class="form-control inputf" type="password" placeholder="Password *" name="pass" required="" style="width: 185px;"></label></td>
-                            <td><label class="form-label inputHead">Confirm Password *<input class="form-control inputf" type="password" placeholder="Confirm Password *" name="cpass" required="" style="width: 185px;"></label></td>
-                        </tr>
-                        <tr class="d-xxl-flex justify-content-xxl-start" style="width: 460px;">
-                            <td class="d-xxl-flex align-items-xxl-center" style="width: 190px;"><button class="btn btn-primary submitBtn" id="submitBtn" type="submit" style="margin-right: 0px;margin-left: 10px;">Next</button></td>
-                            <td class="d-xxl-flex align-items-xxl-center" style="width: 270px;">
-                                <h1 class="secondHead" style="margin-top: 0px;margin-bottom: 0px;font-size: 14px;padding-left: 0px;"><strong>Already got an account?</strong><a id="login" href="#loginpage" style="padding-bottom: 0px;">Login</a></h1>
-                            </td>
-                        </tr>
-                        <tr></tr>
-                        </tbody>
-                    </table>
+            <form class="signupForm" method="POST" action="">
+                <div style="overflow: visible;">
+                    <div>
+                        <div class="d-xxl-flex justify-content-xxl-center">
+                            <div class="<?php echo $error_code===1?'error':'' ?>"><label class="form-label inputHead"><input class="form-control inputf" type="text" name="fname" required placeholder="John" style="width: 220px;"><p class="float-placeholder">First Name <span>*</span></p><p class="error-text"><?php echo $error_code===1?'Name you typed is invalid':'' ?></p></label></div>
+                            <div class="<?php echo $error_code===2?'error':'' ?>"><label class="form-label inputHead"><input class="form-control inputf" type="text" name="lname" required placeholder="Lark" style="width: 220px;"><p class="float-placeholder">Last Name <span>*</span></p><p class="error-text"><?php echo $error_code===2?'Name you typed is invalid':'' ?></p></label></div>
+                        </div>
+                        <div class="d-xxl-flex justify-content-xxl-center">
+                            <div class="<?php echo $error_code===4 || $error_code===14?'error':'' ?>"><label class="form-label inputHead"><input class="form-control inputf" type="email" name="email" required style="width: 480px;" placeholder="example123@email.com"><p class="float-placeholder">Email <span>*</span></p><p class="error-text"><?php echo $error_code === 4 ? 'Email address you provided is invalid' : ($error_code === 14 ? 'An account with this email exists!' : '') ?></p></label></div>
+                        </div>
+                        <div class="d-xxl-flex justify-content-xxl-center">
+                            <div class="<?php echo $error_code===10 || $error_code===12?'error':'' ?>"><label class="form-label inputHead"><input class="form-control inputf" type="password" name="pass" required style="width: 220px;"><p class="float-placeholder">Password <span>*</span></p><p class="error-text"><?php echo $error_code===10? 'This field can\'t be empty!' : ($error_code===12 ? 'Password you provided was invalid' : '' ) ?></p></label></div>
+                            <div class="<?php echo $error_code===9 || $error_code===11?'error':'' ?>"><label class="form-label inputHead"><input class="form-control inputf" type="password" name="cpass" required style="width: 220px;"><p class="float-placeholder">Confirm Password <span>*</span></p><p class="error-text"><?php echo $error_code===9?'The passwords did not match':($error_code===11? 'This field can\'t be empty!': '') ?></p></label></div>
+                        </div>
+                        <div class="d-xxl-flex justify-content-xxl-center">
+                            <div class="<?php echo $error_code==3 || $error_code===13?'error':'' ?>"><label class="form-label inputHead"><input class="form-control inputf" type="text" name="username" required placeholder="_uSer_123_" style="width: 480px;"><p class="float-placeholder">Username <span>*</span></p><p class="error-text"><?php echo $error_code === 3 ? 'Username you typed is invalid' : ($error_code === 13 ? 'This username is taken!' : '') ?></p></label></div>
+                        </div>
+                        <div class="d-xxl-flex justify-content-xxl-center">
+                            <div class="<?php echo $error_code===5 || $error_code===15?'error':'' ?>"><label class="form-label inputHead"><input class="form-control inputf" type="tel" name="phone" required placeholder="+XXX (XXX) XXX XXXX" style="width: 220px;"><p class="float-placeholder">Phone number <span>*</span></p><p class="error-text"><?php echo $error_code===5?'Phone number you typed is invalid!': ($error_code===15?'An account with this number exists!':'') ?></p></label></div>
+                            <div class="<?php echo $error_code===6 || $error_code===7 || $error_code===8?'error':'' ?>"><label class="form-label inputHead"><input class="form-control inputf" type="date" name="dob" required style="width: 220px;"><p class="float-placeholder">Date of birth <span>*</span></p><p class="error-text"><?php if($error_code===6){
+                                echo 'Invalid format!';
+                            }elseif ($error_code===7){
+                                echo 'You must be at least 13 years';
+                            }elseif($error_code===8){
+                                echo 'No one over 100 years of age is allowed!';
+                            }else{
+                                echo '';
+                            } ?></p></label></div>
+                        </div>
+                        <div class="d-xxl-flex justify-content-xxl-evenly">
+                            <div class="d-xxl-flex align-items-xxl-center" style="width: 190px;"><button class="btn btn-primary submitBtn" id="submitBtn" type="submit" style="margin-right: 0px;margin-left: 10px;">Next</button></div>
+                            <div class="d-xxl-flex align-items-xxl-center">
+                                <h1 class="secondHead" style="margin-top: 0px;margin-bottom: 0px;font-size: 14px;padding-left: 0px;"><strong>Already got an account?</strong><a id="login" href="<?php echo $link->path('login_page') ?>" style="padding-bottom: 0px;">Login</a></h1>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </form>
         </div>
---
+++ backend/model/account_model.php
@@ -0,0 +1,277 @@
+<?php
+    include_once "config/db.php";
+
+    // Class with the connection to the _database and functions to interact with the _database
+    class AccountModel{
+        // Variables for the connection to the _database
+        private PDO $connection;
+        private string $_servername = HOST; // Host or Server address
+        private string $_username = USER; // Username of the user accessing the database
+        private string $_password = PSK; // Password for the username accessing the database
+        private string $_database = DB; // Database name
+
+        function __construct(){
+            try {
+                $this->connection = new PDO("mysql:host=$this->_servername;dbname=$this->_database", $this->_username, $this->_password);
+                // set the PDO error mode to exception
+                $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                // echo "Connected successfully";
+            } catch(PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+                // Change error output later
+            }
+        }
+        /*
+        *   Model functions for the account operations
+
+            function returns:
+                true - exists/pass(Based on the situation)
+                false - does not exist/not pass(Based on the situation)
+                0 - success/executed without an error
+                -1 - error/failed to execute
+        */
+
+        // Model function to create a user account
+        function create_account($firstname, $lastname, $username, $dob, $phone_no, $email_address){
+            try{
+                $stmt = $this->connection->prepare("INSERT INTO users (firstname, lastname, DOB, username, phone_no, email_address) VALUES (:firstname, :lastname, :dob, :username, :phone_no, :email_address)");
+                $stmt->bindParam(":firstname", $firstname);
+                $stmt->bindParam(":lastname", $lastname);
+                $stmt->bindParam(":dob", $dob);
+                $stmt->bindParam(":username", $username);
+                $stmt->bindParam(":phone_no", $phone_no);
+                $stmt->bindParam(":email_address", $email_address);
+                $stmt->execute();
+                // Return user id
+                $stmt = $this->connection->prepare("SELECT ID FROM users WHERE username = :username");
+                $stmt->bindParam(":username", $username);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                return $result['ID'];
+            } catch (PDOException $e){
+                echo "PDO(MySQL) Error: " . $e->getMessage();
+                return -1;
+            }
+        }
+        // Model function to check if the username is already taken
+        function check_username($username){
+            try{
+                $stmt = $this->connection->prepare("SELECT ID FROM users WHERE username = :username");
+                $stmt->bindParam(":username", $username);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                if($result['ID'] !== null){
+                    return $result['ID'];
+                }else{
+                    return false;
+                }
+            } catch(PDOException $e){
+                echo "PDO(MySQL) Error: " . $e->getMessage();
+                return -1;
+            }
+        }
+        // Model function to check if the email is already taken
+        function check_email($email){
+            try{
+                $stmt = $this->connection->prepare("SELECT ID FROM users WHERE email_address = :email");
+                $stmt->bindParam(":email", $email);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                if($result['ID'] !== null){
+                    return $result['ID'];
+                }else{
+                    return false;
+                }
+            } catch(PDOException $e){
+                echo "PDO(MySQL) Error: " . $e->getMessage();
+                return -1;
+            }
+        }
+        // Model function to check if the phone number is already taken
+        function check_phone($phone){
+            try{
+                $stmt = $this->connection->prepare("SELECT ID FROM users WHERE phone_no = :phone");
+                $stmt->bindParam(":phone", $phone);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                if($result['ID'] !== null){
+                    return $result['ID'];
+                }else{
+                    return false;
+                }
+            } catch(PDOException $e){
+                echo "PDO(MySQL) Error: " . $e->getMessage();
+                return -1;
+            }
+        }
+        // Model function to check if the user is banned
+        function check_ban($u_id){
+            try{
+                $stmt = $this->connection->prepare("SELECT reason_ID FROM banned_users WHERE u_ID = :u_id AND is_valid = 1");
+                $stmt->bindParam(":u_id", $u_id);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                if($result['reason_ID'] !== null){
+                    return $result['reason_ID'];
+                }else{
+                    return false;
+                }
+            } catch(PDOException $e){
+                echo "PDO(MySQL) Error: " . $e->getMessage();
+                return -1;
+            }
+        }
+        // Model function to get the reason for which the user was banned
+        function get_ban_reason($reason_id)
+        {
+            try {
+                $stmt = $this->connection->prepare("SELECT reason FROM ban_reason WHERE reason_ID = :reason_ID");
+                $stmt->bindParam(":reason_ID", $reason_id);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                return $result['reason'];
+            } catch (PDOException $e) {
+                echo "PDO(MySQL) Error: " . $e->getMessage();
+                return -1;
+            }
+        }
+        // Model function to create a login session
+        function set_login_session($u_id, $cookie_string){
+            try{
+                $stmt = $this->connection->prepare("INSERT INTO login_sessions (u_ID, cookie_string) VALUES (:u_id, :cookie_string)");
+                $stmt->bindParam(":u_id", $u_id);
+                $stmt->bindParam(":session_id", $cookie_string);
+                $stmt->execute();
+                // Fetch the session ID
+                $stmt = $this->connection->prepare("SELECT ID FROM login_sessions WHERE cookie_string = :cookie_string");
+                $stmt->bindParam(":cookie_string", $cookie_string);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                if ($result['ID'] !== null) {
+                    return $result['ID'];
+                } else {
+                    return false;
+                }
+            } catch (PDOException $e){
+                echo "PDO(MySQL) Error: " . $e->getMessage();
+                return -1;
+            }
+        }
+        // Model function to get details of the user's current logged sessions
+        function get_login_session($cookie_string){
+            try{
+                $stmt = $this->connection->prepare("SELECT u_ID FROM login_sessions WHERE cookie_string = :cookie_string AND is_active = 1");
+                $stmt->bindParam(":cookie_string", $cookie_string);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                if($result['u_ID']){
+                    return $result['u_ID'];
+                }else{
+                    return false;
+                }
+            } catch(PDOException $e){
+                echo "PDO(MySQL) Error: " . $e->getMessage();
+                return -1;
+            }
+        }
+        // Model function to log out of the current session
+        function logout_session($cookie_string): int{
+            try{
+                $stmt = $this->connection->prepare("UPDATE login_sessions SET is_active = 0 WHERE cookie_string = :cookie_string");
+                $stmt->bindParam(":cookie_string", $cookie_string);
+                $stmt->execute();
+                return 0;
+            } catch(PDOException $e){
+                echo "PDO(MySQL) Error: " . $e->getMessage();
+                return -1;
+            }
+        }
+        // Model function to get the user's details
+        function get_user_details($u_id){
+            try{
+                $stmt = $this->connection->prepare("SELECT * FROM users WHERE u_ID = :u_id");
+                $stmt->bindParam(":u_id", $u_id);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                if($result){
+                    return $result;
+                }else{
+                    return false;
+                }
+            } catch(PDOException $e){
+                echo "PDO(MySQL) Error: " . $e->getMessage();
+                return -1;
+            }
+        }
+        // Model function to get the user's profile picture
+        function get_user_profile_picture($u_id){
+            try{
+                $stmt = $this->connection->prepare("SELECT path FROM users WHERE u_ID = :u_id AND is_active = 1");
+                $stmt->bindParam(":u_id", $u_id);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                if($result){
+                    return $result['path'];
+                }else{
+                    return false;
+                }
+            } catch(PDOException $e){
+                echo "PDO(MySQL) Error: " . $e->getMessage();
+                return -1;
+            }
+        }
+        // Model function to change the user's profile picture
+        function change_user_profile_picture($u_id, $path) : int{
+            try{
+                // Deactivate the current profile picture
+                $stmt = $this->connection->prepare("UPDATE user_profile_picture SET is_active = 0 WHERE u_ID = :u_id AND is_active = 1");
+                $stmt->bindParam(":u_id", $u_id);
+                $stmt->execute();
+                // Insert the new profile picture
+                $stmt = $this->connection->prepare("INSERT INTO user_profile_picture (u_ID, path) VALUES (:u_id, :path)");
+                $stmt->bindParam(":u_id", $u_id);
+                $stmt->bindParam(":path", $path);
+                $stmt->execute();
+                return 0;
+            } catch(PDOException $e){
+                echo "PDO(MySQL) Error: " . $e->getMessage();
+                return -1;
+            }
+        }
+        // Model function to get the user's password
+        function get_password($u_id){
+            try{
+                $stmt = $this->connection->prepare("SELECT password FROM password WHERE u_ID = :u_id AND is_active = 1");
+                $stmt->bindParam(":u_id", $u_id);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                if($result){
+                    return $result['password'];
+                }else{
+                    return false;
+                }
+            } catch(PDOException $e){
+                echo "PDO(MySQL) Error: " . $e->getMessage();
+                return -1;
+            }
+        }
+        // Model function to change user's password
+        function set_password($u_id, $password) : int{
+            try {
+                // Deactivate the current password
+                $stmt = $this->connection->prepare("UPDATE password SET is_active = 0 WHERE u_ID = :u_id AND is_active = 1");
+                $stmt->bindParam(":u_id", $u_id);
+                $stmt->execute();
+                // Set the new password
+                $stmt = $this->connection->prepare("INSERT INTO password (u_ID, password) VALUES (:u_id, :password)");
+                $stmt->bindParam(":u_id", $u_id);
+                $stmt->bindParam(":password", $password);
+                $stmt->execute();
+                return 0;
+            } catch (PDOException $e) {
+                echo "PDO(MySQL) Error: " . $e->getMessage();
+                return -1;
+            }
+        }
+
+    }
